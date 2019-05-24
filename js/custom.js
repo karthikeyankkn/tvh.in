91552cb83f09cbe861b55147694ffa1dea5a957e
baskarbss <baskar@spinircle.com>
2019-05-24 23:32:24 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,83 @@
+(function ($) {
+  "use strict";
+
+  var review = $('.review_part_text');
+  if (review.length) {
+    review.owlCarousel({
+      items: 2,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      margin: 40,
+      autoplayHoverPause: true,
+      autoplayTimeout:5000,
+      nav: false,
+      responsive: {
+        0:{
+          items: 1
+        },
+        480:{
+          items: 1
+        },
+        769:{
+          items: 2
+        }
+    }
+    });
+  }
+  $('.popup-youtube, .popup-vimeo').magnificPopup({
+    // disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
+  // menu fixed js code
+  $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.main_menu').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
+  if (document.getElementById('default-select')) {
+		$('select').niceSelect();
+	}
+
+  // page-scroll
+  $('.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    var headerH = '80';
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+ });
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+
+}(jQuery));
